12ecad7c837f3247774206371d4797c080cdbcab
slapp <slappedtoast@gmail.com>
2024-11-15 13:51:35 -0800

    modified servo test and arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Arm {
+
+    private DcMotorEx motor0;
+    private DcMotorEx motor1;
+
+    public Arm(DcMotorEx motor0, DcMotorEx motor1, Gamepad gamepad) {
+        this.motor0 = motor0;
+        this.motor1 = motor1;
+
+        
+    }
+
+
+
+
+}
