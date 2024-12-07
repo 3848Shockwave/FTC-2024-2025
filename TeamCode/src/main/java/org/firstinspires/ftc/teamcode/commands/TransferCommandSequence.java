5f35c94eace7907777c5882867b65d91b568a28d
slapp <slappedtoast@gmail.com>
2024-12-06 17:37:54 -0800

    test comment transfer command sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TransferCommandSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TransferCommandSequence.java
@@ -26,33 +26,33 @@ public class TransferCommandSequence extends SequentialCommandGroup {
         this.intakeSubsystem = intakeSubsystem;
         this.telemetry = telemetry;
         addCommands(
-                // set vertical arm to transfer position
-                new SetVerticalArmPositionCommand(intakeSubsystem, IntakeSubsystem.IntakeState.TRANSFER),
-//                // (wait until ^ done)
-                new WaitCommand(WAIT0),
-                // set horizontal arm to transfer position
-                new SetHorizontalArmPositionCommand(intakeSubsystem, IntakeSubsystem.IntakeState.TRANSFER),
-                new WaitCommand(WAIT1),
-//                // close vertical arm claw
-                new InstantCommand(intakeSubsystem::closeVerticalClaw),
-//                // (wait until ^ done)
-                new WaitCommand(WAIT2),
-//                // open horizontal arm claw
-                new InstantCommand(intakeSubsystem::openHorizontalClaw),
-//                // (wait until ^ done)
-                new WaitCommand(WAIT3),
-
-                // set vertical slide position to deposit position, after start of this command: wait 500 ms, then set vertical arm to deposit position
-                new ParallelCommandGroup(
-                        // set vertical slide position to transfer position
-                        new SetVerticalSlidePositionCommand(intakeSubsystem, Constants.VERTICAL_SLIDE_MOTOR_DEPOSIT_POSITION, Constants.TEST_DOUBLE, telemetry),
-                        new SequentialCommandGroup(
-                                new WaitCommand(WAIT4),
-                                // set vertical arm to deposit position
-                                new SetVerticalArmPositionCommand(intakeSubsystem, IntakeSubsystem.IntakeState.DEPOSIT)
-                        )
-                )
-                // DONE!
+//                // set vertical arm to transfer position
+//                new SetVerticalArmPositionCommand(intakeSubsystem, IntakeSubsystem.IntakeState.TRANSFER),
+////                // (wait until ^ done)
+//                new WaitCommand(WAIT0),
+//                // set horizontal arm to transfer position
+//                new SetHorizontalArmPositionCommand(intakeSubsystem, IntakeSubsystem.IntakeState.TRANSFER),
+//                new WaitCommand(WAIT1),
+////                // close vertical arm claw
+//                new InstantCommand(intakeSubsystem::closeVerticalClaw),
+////                // (wait until ^ done)
+//                new WaitCommand(WAIT2),
+////                // open horizontal arm claw
+//                new InstantCommand(intakeSubsystem::openHorizontalClaw),
+////                // (wait until ^ done)
+//                new WaitCommand(WAIT3),
+//
+//                // set vertical slide position to deposit position, after start of this command: wait 500 ms, then set vertical arm to deposit position
+//                new ParallelCommandGroup(
+//                        // set vertical slide position to transfer position
+//                        new SetVerticalSlidePositionCommand(intakeSubsystem, Constants.VERTICAL_SLIDE_MOTOR_DEPOSIT_POSITION, Constants.TEST_DOUBLE, telemetry),
+//                        new SequentialCommandGroup(
+//                                new WaitCommand(WAIT4),
+//                                // set vertical arm to deposit position
+//                                new SetVerticalArmPositionCommand(intakeSubsystem, IntakeSubsystem.IntakeState.DEPOSIT)
+//                        )
+//                )
+//                // DONE!
         );
         addRequirements(intakeSubsystem);
     }
